361bc2276ba7a4be857e717a240fadb549895679
Aquilaruspante <calogero.salvaggio3@gmail.com>
2026-06-16 21:14:51 +0100

    feat(new_message): add form and relative get and post routes

--- app.js
+++ app.js
@@ -17,15 +17,30 @@ const messages = [
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
     res.render('index', { messages });
-})
+});
+
+app.get('/new', (req, res) => {
+    res.render('form');
+});
+
+app.post('/new', (req, res) => {
+    const { user, message } = req.body;
+    messages.push({ text: message, user, added: new Date() });
+    res.redirect('/');
+});
+
+app.all('/{*splat}', (req, res) => {
+    res.status(404).send('Page not found!');
+});
 
 app.listen(3000, (error) => {
-    if (error) return console.log('Failed to start server');
+    if (error) return console.log('Failed to start server', error.message);
     console.log('Server started at http://127.0.0.1:3000');
 });
