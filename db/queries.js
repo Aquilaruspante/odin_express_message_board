55b073af2adfffa4bbc21f8d11018e8e22932b95
Aquilaruspante <calogero.salvaggio3@gmail.com>
2026-06-28 21:26:48 +0100

    feat(db) add queries

---
+++ db/queries.js
@@ -0,0 +1,20 @@
+const pool = require('./pool');
+
+function randomColor() {
+    const index = Math.floor(Math.random() * colors.length - 1);
+    return colors[index];
+};
+
+async function getMessages() {
+    const { rows } = await pool.query('SELECT * FROM messages;');
+    return rows;
+};
+
+async function createMessage(message, user) {
+    const color = randomColor();
+    await pool.query(`
+            INSERT INTO messages (text, username, added, color)
+            VALUES ('${message}', '${user}', '${new Date()}', '${color}');
+        `
+    );
+}
