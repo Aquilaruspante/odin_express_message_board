c23abc5f01b5a23f4cc5648910675d682eb8b210
Aquilaruspante <calogero.salvaggio3@gmail.com>
2026-06-28 21:17:52 +0100

    feat(db) add pool

---
+++ db/pool.js
@@ -0,0 +1,11 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+    user: process.env.USERNAME,
+    password: process.env.PASSWORD,
+    host: process.env.HOST,
+    port: process.env.PORT,
+    database: process.env.DATABASE
+});
+
+module.exports = pool;
